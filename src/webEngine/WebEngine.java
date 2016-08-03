f128cc9a8eb445d098dc756a7f47760748dfa55d
shaypastakia <shaypastakia@gmail.com>
2016-08-03 10:48:29 +0100

    Reduced animation delay for recipe requests

--- src/webEngine/WebEngine.java
+++ src/webEngine/WebEngine.java
@@ -107,9 +107,12 @@ public class WebEngine extends HttpServlet{
 		
 		String html = "";
 		html += "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">" +
-		"<html xmlns=\"http://www.w3.org/1999/xhtml\"><head>" + style + style2 + style3
+		"<html xmlns=\"http://www.w3.org/1999/xhtml\"><head>" + style + style2;// + style3
 
-		+ "<meta content=\"en-gb\" http-equiv=\"Content-Language\" />" + 
+		if (!request.getParameterMap().containsKey("title"))
+			html += style3; 
+		
+		html += "<meta content=\"en-gb\" http-equiv=\"Content-Language\" />" + 
 		"<meta content=\"text/html; charset=utf-8\" http-equiv=\"Content-Type\" />" + 
 		"<title>Shokuhin</title><style type=\"text/css\">.auto-style2 {font-family: \"DejaVu Sans\";color:black;border-collapse: collapse}" +
 		"</style></head><body background=" + request.getContextPath() + "/images/" + image + " link=\"#000000\" vlink=\"#000000\" alink=\"#000000\" bgcolor=\"#DDDDDD\">"
