de5035709693603461dd8a58fc332209dddcd55a
shaypastakia <shaypastakia@gmail.com>
2017-02-26 06:47:19 +0000

    initial attempt to send attachment and link

--- src/webEngine/SMTPEngine.java
+++ src/webEngine/SMTPEngine.java
@@ -1,14 +1,28 @@
 package webEngine;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
 import java.util.Properties;
 
+import javax.activation.DataHandler;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.Multipart;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+import org.apache.commons.httpclient.util.URIUtil;
 
 import recipe.Recipe;
 
@@ -33,15 +47,46 @@ public class SMTPEngine {
 		prop.put("mail.smtp.starttls.enable", "true");
 	}
 	
-	public void sendRecipe(Recipe r, String recipient) throws AddressException, MessagingException{
+	public void sendRecipe(Recipe r, String recipient) throws AddressException, MessagingException, IOException{
 		
 		//code from http://crunchify.com/java-mailapi-example-send-an-email-via-gmail-smtp/
 		session = Session.getDefaultInstance(prop, null);
 		message = new MimeMessage(session);
 		message.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
 		
-		String body = "Hi";
-		message.setContent(body, "text/html");
+		String bodyText = "To view this recipe on the Shokuhin website, please click this link:\n"
+				+ "<a href=\"http://www.shokuhin.herokuapp.com/shokuhin?title="
+				+ URIUtil.encodeAll(r.getTitle())
+				+ "\">"
+				+ r.getTitle() + "</a><br /><br />"
+				+ "You will also find the recipe attached to this email.";
+		message.setSubject("Recipe for " + r.getTitle());
+//		message.setContent(bodyText, "text/html");
+		
+		BodyPart bodyPart = new MimeBodyPart();
+		bodyPart.setContent(bodyText, "text/html");
+		Multipart multi = new MimeMultipart();
+		multi.addBodyPart(bodyPart);
+		
+		BodyPart attachment = new MimeBodyPart();
+		
+		File tempFile = File.createTempFile(r.getTitle(),".html");
+		tempFile.deleteOnExit();
+		
+		OutputStream fileOut = Files.newOutputStream(tempFile.toPath());
+		BufferedOutputStream buff = new BufferedOutputStream(fileOut);
+		ObjectOutputStream obj = new ObjectOutputStream(buff);
+		obj.writeObject(r);
+		obj.close();
+		buff.close();
+		fileOut.close();
+		
+		attachment.setDataHandler(new DataHandler(new FileDataSource(tempFile)));
+		attachment.setFileName(r.getTitle() + ".html");
+		
+		multi.addBodyPart(attachment);
+		
+		message.setContent(multi);
 		
 		Transport transport = session.getTransport("smtp");
 		 
